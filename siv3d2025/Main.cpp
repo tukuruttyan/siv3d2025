789ce98c71bec532c4f8c2748770699892c906ac
henohenon <takeru.hawk.son@gmail.com>
2025-10-27 17:59:52 +0900

    Set window properties and title in `Main` initialization

--- siv3d2025/Main.cpp
+++ siv3d2025/Main.cpp
@@ -4,5 +4,11 @@
 void Main()
 {
 	GameCore::Game game;
+
+	Scene::SetResizeMode(ResizeMode::Keep);
+	Window::SetStyle(WindowStyle::Sizable);
+	Window::Resize(640, 480);
+	Window::SetTitle(U"深海ギョ戦記");
+
 	game.RunAsync();
 }
